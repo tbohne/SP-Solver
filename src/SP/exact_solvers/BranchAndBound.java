a04d1cdaeab87884cdca57a91b1664af52b216b8
tbohne <tbohne@uos.de>
2019-08-05 10:12:54 +0200

    implemented construction of bipartite graph for LBs

--- src/SP/exact_solvers/BranchAndBound.java
+++ src/SP/exact_solvers/BranchAndBound.java
@@ -1,11 +1,19 @@
 package SP.exact_solvers;
 
+import SP.representations.BipartiteGraph;
 import SP.representations.Item;
 import SP.representations.Solution;
+import SP.representations.StackPosition;
+import SP.util.GraphUtil;
 import SP.util.HeuristicUtil;
+import org.jgrapht.Graph;
+import org.jgrapht.graph.DefaultUndirectedWeightedGraph;
+import org.jgrapht.graph.DefaultWeightedEdge;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class BranchAndBound {
 
@@ -67,6 +75,60 @@ public class BranchAndBound {
         actuallySolve(solutionsForItemIteration, 1);
     }
 
+    /**
+     * Adds edges to the bipartite graph that connect items with stack positions.
+     *
+     * @param bipartiteGraph - bipartite graph to add the edges to
+     * @param items          - items to be connected to positions
+     * @param positions      - positions the items get connected to
+     */
+    private void addEdgesBetweenItemsAndStackPositions(
+            Graph<String, DefaultWeightedEdge> bipartiteGraph, List<Integer> items, List<StackPosition> positions
+    ) {
+        for (int item : items) {
+            for (StackPosition pos : positions) {
+                DefaultWeightedEdge edge = bipartiteGraph.addEdge("item" + item, "pos" + pos);
+                double costs = this.costs[item][pos.getStackIdx()];
+                bipartiteGraph.setEdgeWeight(edge, costs);
+            }
+        }
+    }
+
+    /**
+     * Adds edges to the bipartite graph that connect dummy items with stack positions.
+     *
+     * @param bipartiteGraph - bipartite graph to add the edges to
+     * @param dummyItems     - dummy items to be connected to positions
+     * @param positions      - positions the dummy items get connected to
+     */
+    private void addEdgesBetweenDummyItemsAndStackPositions(
+            Graph<String, DefaultWeightedEdge> bipartiteGraph, List<Integer> dummyItems, List<StackPosition> positions
+    ) {
+        for (int item : dummyItems) {
+            for (StackPosition pos : positions) {
+                DefaultWeightedEdge edge = bipartiteGraph.addEdge("dummy" + item, "pos" + pos);
+                bipartiteGraph.setEdgeWeight(edge, 0);
+            }
+        }
+    }
+
+    private BipartiteGraph constructBipartiteGraph(Solution sol) {
+        List<Integer> unassignedItems = sol.getUnassignedItems();
+        List<StackPosition> freePositions = HeuristicUtil.retrieveEmptyPositions(sol);
+
+        Graph<String, DefaultWeightedEdge> graph = new DefaultUndirectedWeightedGraph<>(DefaultWeightedEdge.class);
+        Set<String> partitionOne = new HashSet<>();
+        Set<String> partitionTwo = new HashSet<>();
+
+        GraphUtil.addVerticesForUnmatchedItems(unassignedItems, graph, partitionOne);
+        GraphUtil.addVerticesForEmptyPositions(freePositions, graph, partitionTwo);
+        List<Integer> dummyItems = GraphUtil.introduceDummyVerticesToBipartiteGraph(graph, partitionOne, partitionTwo);
+        this.addEdgesBetweenDummyItemsAndStackPositions(graph, dummyItems, freePositions);
+        this.addEdgesBetweenItemsAndStackPositions(graph, unassignedItems, freePositions);
+
+        return new BipartiteGraph(partitionOne, partitionTwo, graph);
+    }
+
     private void actuallySolve(List<Solution> solutions, int itemToBeAdded) {
 
         List<Solution> newSolutions = new ArrayList<>();
@@ -75,6 +137,10 @@ public class BranchAndBound {
             for (int stack = 0; stack < currSol.getFilledStacks().length; stack++) {
 
                 Solution tmpSol = new Solution(currSol);
+
+                // compute LB for solution
+                // if LB < c*
+                BipartiteGraph graph = this.constructBipartiteGraph(tmpSol);
 
                 if (HeuristicUtil.itemCompatibleWithStack(this.costs, itemToBeAdded, stack)
                     && HeuristicUtil.stackHasFreePosition(tmpSol.getFilledStacks()[stack])
@@ -91,7 +157,7 @@ public class BranchAndBound {
         if (itemToBeAdded + 1 <  this.numberOfItems) {
             this.actuallySolve(newSolutions, ++itemToBeAdded);
         } else {
-            this.printRes(newSolutions);
+//            this.printRes(newSolutions);
         }
     }
 }
