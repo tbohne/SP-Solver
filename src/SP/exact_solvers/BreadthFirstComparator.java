e7cac76407c559360ae539dc2cd99d8d21593b6e
tbohne <tbohne@uos.de>
2019-08-13 09:38:00 +0200

    implemented BFS comparator

--- src/SP/exact_solvers/BreadthFirstComparator.java
+++ src/SP/exact_solvers/BreadthFirstComparator.java
@@ -4,14 +4,14 @@ import SP.representations.Solution;
 
 import java.util.Comparator;
 
-public class DepthFirstComparator implements Comparator<Solution> {
+public class BreadthFirstComparator implements Comparator<Solution> {
 
     @Override
     public int compare(Solution solOne, Solution solTwo) {
         if (solOne.getNumberOfAssignedItems() < solTwo.getNumberOfAssignedItems()) {
-            return 1;
-        } else if (solTwo.getNumberOfAssignedItems() < solOne.getNumberOfAssignedItems()) {
             return -1;
+        } else if (solTwo.getNumberOfAssignedItems() < solOne.getNumberOfAssignedItems()) {
+            return 1;
         }
         return 0;
     }
