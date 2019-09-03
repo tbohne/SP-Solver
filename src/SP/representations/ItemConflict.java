221e2cb851afec570a9e87a2e821190b93b5a798
tbohne <tbohne@uos.de>
2019-09-03 16:02:34 +0200

    implemented representation for item conflicts

---
+++ src/SP/representations/ItemConflict.java
@@ -0,0 +1,34 @@
+package SP.representations;
+
+public class ItemConflict implements Comparable<ItemConflict> {
+
+    private int itemIdx;
+    private int numberOfConflicts;
+
+    public ItemConflict(int idx, int conflicts) {
+        this.itemIdx = idx;
+        this.numberOfConflicts = conflicts;
+    }
+
+    public int getNumberOfConflicts() {
+        return this.numberOfConflicts;
+    }
+
+    public int getItemIdx() {
+        return this.itemIdx;
+    }
+
+    public void setNumberOfConflicts(int conflicts) {
+        this.numberOfConflicts = conflicts;
+    }
+
+    @Override
+    public int compareTo(ItemConflict otherConflict) {
+        if (this.numberOfConflicts < otherConflict.numberOfConflicts) {
+            return -1;
+        } else if (otherConflict.numberOfConflicts < this.numberOfConflicts) {
+            return 1;
+        }
+        return 0;
+    }
+}
