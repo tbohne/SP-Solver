6036c27870c18b43c8c9a05a92df00e05531042c
tbohne <tbohne@uos.de>
2019-10-07 09:16:15 +0200

    updated swap-shift nbh interface

--- src/SP/post_optimization_methods/SwapShiftNeighborhood.java
+++ src/SP/post_optimization_methods/SwapShiftNeighborhood.java
@@ -2,22 +2,11 @@ package SP.post_optimization_methods;
 
 import SP.representations.Solution;
 
-import java.util.List;
-import java.util.Queue;
-
 public interface SwapShiftNeighborhood {
 
-    Solution getNeighborKSwap(int numberOfSwaps, Solution currSol, Queue<Shift> tabuList, Solution bestSol);
+    Solution getNeighborKSwap(int numberOfSwaps, Solution currSol, Solution bestSol);
 
-    Solution getNeighborShift(Solution currSol, Queue<Shift> tabuList, Solution bestSol);
-
-    void forbidShift(Shift shift, Queue<Shift> tabuList);
-
-    void clearTabuList(Queue<Shift> tabuList);
+    Solution getNeighborShift(Solution currSol, Solution bestSol);
 
     int getTabuListClears();
-
-    Solution performSwaps(int numberOfSwaps, List<Swap> swapList, Solution currSol);
-
-    boolean containsTabuSwap(List<Swap> swapList, Queue<Shift> tabuList);
 }
