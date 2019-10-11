1a63ea5630e929cf5fe4f8db7c6e0a4c1283b0d8
tbohne <tbohne@uos.de>
2019-10-11 12:59:36 +0200

    refined TS and config

--- src/SP/experiments/PostOptimization.java
+++ src/SP/experiments/PostOptimization.java
@@ -33,29 +33,32 @@ public class PostOptimization {
         BEST_FIT
     }
 
+    /************************************* CONFIG *************************************/
     private static final String INSTANCE_PREFIX = "res/instances/";
     private static final String SOLUTION_PREFIX = "res/solutions/";
 
+    // GENERAL
     private static final ShortTermStrategies SHORT_TERM_STRATEGY = ShortTermStrategies.BEST_FIT;
     private static final StoppingCriteria STOPPING_CRITERION = StoppingCriteria.NON_IMPROVING_ITERATIONS;
+    private static final int NUMBER_OF_NEIGHBORS = 30;
+    private static final int MAX_TABU_LIST_LENGTH_FACTOR = 10;
+    private static final int UNSUCCESSFUL_NEIGHBOR_GENERATION_ATTEMPTS = 100;
 
+    // STOPPING SPECIFIC
     private static final int NUMBER_OF_ITERATIONS = 50;
     private static final int NUMBER_OF_TABU_LIST_CLEARS = 10;
-    private static final int NUMBER_OF_NON_IMPROVING_ITERATIONS = 50;
+    private static final int NUMBER_OF_NON_IMPROVING_ITERATIONS = 5;
 
-    private static final int NUMBER_OF_NEIGHBORS = 1000;
-    private static final int MAX_TABU_LIST_LENGTH_FACTOR = 9;
-    private static final int UNSUCCESSFUL_NEIGHBOR_GENERATION_ATTEMPTS = 5000;
+    // NBH SPECIFIC
     private static final int UNSUCCESSFUL_K_SWAP_ATTEMPTS = 1000;
     private static final int K_SWAP_INTERVAL_UB = 4;
-
     private static final float K_SWAP_PROBABILITY = 5.0F;
     private static final float SWAP_PROBABILITY = 45.0F;
 
     // 3Cap solutions otherwise
-    private static final boolean OPTIMIZE_TWO_CAP_SOLUTIONS = true;
-
-    private static final boolean USING_STACK_BASED_NEIGHBORHOOD = true;
+    // TODO: shouldn't matter - should work for general b
+    private static final boolean OPTIMIZE_TWO_CAP_SOLUTIONS = false;
+    /**********************************************************************************/
 
     public static void main(String[] args) {
         optimizeSolutions();
@@ -87,11 +90,11 @@ public class PostOptimization {
             double startTime = System.currentTimeMillis();
 
             TabuSearch ts = new TabuSearch(
-                sol.getSol(), sol.getOptimalObjectiveValue(), NUMBER_OF_NEIGHBORS, MAX_TABU_LIST_LENGTH_FACTOR,
-                SHORT_TERM_STRATEGY, UNSUCCESSFUL_NEIGHBOR_GENERATION_ATTEMPTS, UNSUCCESSFUL_K_SWAP_ATTEMPTS,
-                NUMBER_OF_NON_IMPROVING_ITERATIONS, K_SWAP_INTERVAL_UB, NUMBER_OF_ITERATIONS, NUMBER_OF_TABU_LIST_CLEARS,
-                STOPPING_CRITERION, K_SWAP_PROBABILITY, SWAP_PROBABILITY, USING_STACK_BASED_NEIGHBORHOOD
+                sol.getSol(), 0, sol.getOptimalObjectiveValue(), NUMBER_OF_NEIGHBORS, MAX_TABU_LIST_LENGTH_FACTOR,
+                SHORT_TERM_STRATEGY, UNSUCCESSFUL_NEIGHBOR_GENERATION_ATTEMPTS, NUMBER_OF_NON_IMPROVING_ITERATIONS,
+                NUMBER_OF_ITERATIONS, NUMBER_OF_TABU_LIST_CLEARS, STOPPING_CRITERION
             );
+
             Solution impSol = ts.solve();
             impSol.setTimeToSolve((System.currentTimeMillis() - startTime) / 1000.0);
 
