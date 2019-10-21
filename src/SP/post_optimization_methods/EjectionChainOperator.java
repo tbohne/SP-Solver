8cb2b0984617657f9d825fbb674007e0efb44ec7
tbohne <tbohne@uos.de>
2019-10-21 16:14:31 +0200

    implemented source vertex handling from Capua paper

--- src/SP/post_optimization_methods/EjectionChainOperator.java
+++ src/SP/post_optimization_methods/EjectionChainOperator.java
@@ -26,11 +26,9 @@ public class EjectionChainOperator {
         return stackOrder;
     }
 
-    private void addVerticesForItems(Graph<String, DefaultWeightedEdge> graph, int numberOfItems, int source) {
+    private void addVerticesForItems(Graph<String, DefaultWeightedEdge> graph, int numberOfItems) {
         for (int item = 0; item < numberOfItems; item++) {
-            if (item != source) {
-                graph.addVertex("item" + item);
-            }
+            graph.addVertex("item" + item);
         }
     }
 
@@ -40,12 +38,6 @@ public class EjectionChainOperator {
                 graph.addVertex("stack" + stack);
             }
         }
-    }
-
-    private int addRandomSourceVertex(Graph<String, DefaultWeightedEdge> graph, int numberOfItems) {
-        int randomItem = HeuristicUtil.getRandomIntegerInBetween(0, numberOfItems - 1);
-        graph.addVertex("source" + randomItem);
-        return randomItem;
     }
 
     private double getCostsForStack(Solution currSol, int stack) {
@@ -59,7 +51,7 @@ public class EjectionChainOperator {
     }
 
     private void addEdgeBetweenItems(
-            DefaultDirectedWeightedGraph<String, DefaultWeightedEdge> graph, Solution currSol, int itemOne, int itemTwo
+        DefaultDirectedWeightedGraph<String, DefaultWeightedEdge> graph, Solution currSol, int itemOne, int itemTwo
     ) {
 
         if (!graph.containsEdge("item" + itemOne, "item" + itemTwo)) {
@@ -67,8 +59,8 @@ public class EjectionChainOperator {
             int stackIdxItemTwo = currSol.getStackIdxForAssignedItem(itemTwo);
 
             if (HeuristicUtil.itemCompatibleWithAlreadyAssignedItemsWithException(
-                    itemOne, currSol.getFilledStacks()[stackIdxItemTwo], currSol.getSolvedInstance().getItemObjects(),
-                    currSol.getSolvedInstance().getStackingConstraints(), itemTwo
+                itemOne, currSol.getFilledStacks()[stackIdxItemTwo], currSol.getSolvedInstance().getItemObjects(),
+                currSol.getSolvedInstance().getStackingConstraints(), itemTwo
             )) {
 
                 if (HeuristicUtil.itemCompatibleWithStack(currSol.getSolvedInstance().getCosts(), itemOne, stackIdxItemTwo)) {
@@ -90,7 +82,7 @@ public class EjectionChainOperator {
     }
 
     private void addEdgeFromItemToStack(
-            DefaultDirectedWeightedGraph<String, DefaultWeightedEdge> graph, Solution currSol, int item, int stack
+        DefaultDirectedWeightedGraph<String, DefaultWeightedEdge> graph, Solution currSol, int item, int stack
     ) {
 
         if (graph.containsVertex("stack" + stack) && graph.containsVertex("item" + item)) {
@@ -98,8 +90,8 @@ public class EjectionChainOperator {
             if (HeuristicUtil.itemCompatibleWithStack(currSol.getSolvedInstance().getCosts(), item, stack)) {
 
                 if (HeuristicUtil.itemCompatibleWithAlreadyAssignedItems(item, currSol.getFilledStacks()[stack],
-                        currSol.getSolvedInstance().getItemObjects(), currSol.getSolvedInstance().getStackingConstraints())
-                        ) {
+                    currSol.getSolvedInstance().getItemObjects(), currSol.getSolvedInstance().getStackingConstraints())
+                ) {
 
                     DefaultWeightedEdge edge = graph.addEdge("item" + item, "stack" + stack);
                     double currCosts = getCostsForStack(currSol, stack);
@@ -116,7 +108,7 @@ public class EjectionChainOperator {
     }
 
     private void addEdgeFromStackToItem(
-            DefaultDirectedWeightedGraph<String, DefaultWeightedEdge> graph, Solution currSol, int stack, int item
+        DefaultDirectedWeightedGraph<String, DefaultWeightedEdge> graph, Solution currSol, int stack, int item
     ) {
 
         if (graph.containsVertex("stack" + stack) && graph.containsVertex("item" + item)) {
@@ -133,41 +125,8 @@ public class EjectionChainOperator {
         }
     }
 
-    private void addEdgeBetweenSourceAndItem(
-            DefaultDirectedWeightedGraph<String, DefaultWeightedEdge> graph, Solution currSol, int source, int item
-    ) {
-
-        if (HeuristicUtil.itemCompatibleWithAlreadyAssignedItemsWithException(
-                source, currSol.getFilledStacks()[currSol.getStackIdxForAssignedItem(item)],
-                currSol.getSolvedInstance().getItemObjects(), currSol.getSolvedInstance().getStackingConstraints(), item
-        )) {
-
-            if (HeuristicUtil.itemCompatibleWithStack(currSol.getSolvedInstance().getCosts(), source, currSol.getStackIdxForAssignedItem(item))) {
-
-                if (graph.containsVertex("item" + item)) {
-
-                    DefaultWeightedEdge edge = graph.addEdge("source" + source, "item" + item);
-                    int stack = currSol.getStackIdxForAssignedItem(item);
-                    double currCosts = getCostsForStack(currSol, stack);
-                    double afterReduction = currCosts - currSol.getSolvedInstance().getCosts()[item][stack];
-
-                    double sourceCostBefore = currSol.getSolvedInstance().getCosts()[source][currSol.getStackIdxForAssignedItem(source)];
-                    double sourceCostAfter = currSol.getSolvedInstance().getCosts()[source][stack];
-                    double sourceDiff = sourceCostBefore - sourceCostAfter;
-
-                    // cost reduction when positive
-                    double costDiff = currCosts - afterReduction + sourceDiff;
-                    // negating edge costs to obtain longest path using Bellman-Ford
-                    costDiff *= -1;
-                    graph.setEdgeWeight(edge, costDiff);
-                }
-            }
-        }
-    }
-
     private void addEdges(
-            DefaultDirectedWeightedGraph<String, DefaultWeightedEdge> graph, List<Integer> stackOrder,
-            int numberOfItems, Solution currSol, int source
+        DefaultDirectedWeightedGraph<String, DefaultWeightedEdge> graph, List<Integer> stackOrder, int numberOfItems, Solution currSol
     ) {
 
         for (int i = 0; i < numberOfItems; i++) {
@@ -201,21 +160,22 @@ public class EjectionChainOperator {
         }
 
         for (int item = 0; item < numberOfItems; item++) {
-            if (currSol.getStackIdxForAssignedItem(item) == currSol.getStackIdxForAssignedItem(source)) { continue; }
-            addEdgeBetweenSourceAndItem(graph, currSol, source, item);
+            DefaultWeightedEdge edge = graph.addEdge("source", "item" + item);
+            int itemStack = currSol.getStackIdxForAssignedItem(item);
+            double currCosts = getCostsForStack(currSol, itemStack);
+            double afterReduction = currCosts - currSol.getSolvedInstance().getCosts()[item][itemStack];
+            // cost reduction when positive
+            double costDiff = currCosts - afterReduction;
+            // negating edge costs to obtain longest path using Bellman-Ford
+            costDiff *= -1;
+            graph.setEdgeWeight(edge, costDiff);
         }
     }
 
     private void applySourceToItemEdge(String lhs, String rhs, Solution currSol, List<Shift> performedShifts) {
-        int source = Integer.parseInt(lhs.replace("(source", "").trim());
-        int stackOfSource = currSol.getStackIdxForAssignedItem(source);
-        HeuristicUtil.removeItemFromStack(source, currSol.getFilledStacks()[stackOfSource]);
         int item = Integer.parseInt(rhs.replace("item", "").replace(")", "").trim());
         int stackOfItem = currSol.getStackIdxForAssignedItem(item);
-
         HeuristicUtil.removeItemFromStack(item, currSol.getFilledStacks()[stackOfItem]);
-        performedShifts.add(new Shift(source, stackOfItem));
-        HeuristicUtil.assignItemToStack(source, currSol.getFilledStacks()[stackOfItem], currSol.getSolvedInstance().getItemObjects());
     }
 
     private void applyItemToItemEdge(String lhs, String rhs, Solution currSol, List<Shift> performedShifts) {
@@ -252,12 +212,11 @@ public class EjectionChainOperator {
     }
 
     private PendingItemStackAssignment applyStackToItemEdge(
-            String lhs, String rhs, Solution currSol, PendingItemStackAssignment pending, List<Shift> performedShifts
+        String lhs, String rhs, Solution currSol, PendingItemStackAssignment pending, List<Shift> performedShifts
     ) {
 
         int stack = Integer.parseInt(lhs.replace("(stack", "").trim());
         int item = Integer.parseInt(rhs.replace("item", "").replace(")", "").trim());
-
         HeuristicUtil.removeItemFromStack(item, currSol.getFilledStacks()[stack]);
 
         if (pending != null && stack == pending.getStack()) {
@@ -296,15 +255,15 @@ public class EjectionChainOperator {
     }
 
     public GraphPath getBestPath(
-            DefaultDirectedWeightedGraph<String, DefaultWeightedEdge> graph,
-            BellmanFordShortestPath shortestPath, int numberOfStacks, int source
+        DefaultDirectedWeightedGraph<String, DefaultWeightedEdge> graph,
+        BellmanFordShortestPath shortestPath, int numberOfStacks
     ) {
 
         GraphPath bestPath = null;
 
         for (int stack = 0; stack < numberOfStacks; stack++) {
             if (graph.containsVertex("stack" + stack)) {
-                GraphPath path = shortestPath.getPath("source" + source, "stack" + stack);
+                GraphPath path = shortestPath.getPath("source", "stack" + stack);
                 if (path != null) {
                     if (bestPath != null) {
                         // looking for the shortest path --> biggest reduction
@@ -326,17 +285,17 @@ public class EjectionChainOperator {
 
         // construct auxiliary graph:
         DefaultDirectedWeightedGraph<String, DefaultWeightedEdge> graph = new DefaultDirectedWeightedGraph<>(DefaultWeightedEdge.class);
-        int source = addRandomSourceVertex(graph, currSol.getSolvedInstance().getItems().length);
-        addVerticesForItems(graph, currSol.getSolvedInstance().getItems().length, source);
+        graph.addVertex("source");
+        addVerticesForItems(graph, currSol.getSolvedInstance().getItems().length);
         addVerticesForStacks(graph, currSol.getFilledStacks().length, currSol);
-        addEdges(graph, stackOrder, currSol.getSolvedInstance().getItems().length, currSol, source);
+        addEdges(graph, stackOrder, currSol.getSolvedInstance().getItems().length, currSol);
 
         // compute shortest path from source to any node in V^ZERO (stacks)
         // --> sequence of combined item insertions and removals
         // each shortest path from source to a node in V^ZERO corresponds to one ejection chain --> choose best
         BellmanFordShortestPath shortestPath = new BellmanFordShortestPath(graph);
 
-        return getBestPath(graph, shortestPath, stackOrder.size(), source);
+        return getBestPath(graph, shortestPath, stackOrder.size());
     }
 
     public Solution generateEjectionChainNeighbor(Solution currSol, List<Shift> performedShifts) {
